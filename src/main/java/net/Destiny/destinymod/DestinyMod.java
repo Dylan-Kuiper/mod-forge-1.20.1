e9d158b0a53f1b625278a0f9749617d5c0bc03a2
Dylan Kuiper <165335@student.horizoncollege.nl>
2023-12-06 12:37:51 +0100

    add first item

--- src/main/java/net/Destiny/destinymod/DestinyMod.java
+++ src/main/java/net/Destiny/destinymod/DestinyMod.java
@@ -1,6 +1,9 @@
 package net.Destiny.destinymod;
 
 import com.mojang.logging.LogUtils;
+import net.Destiny.destinymod.item.Moditems;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -17,14 +20,14 @@ import org.slf4j.Logger;
 @Mod(DestinyMod.MOD_ID)
 public class DestinyMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "destinymod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public DestinyMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        Moditems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -38,7 +41,9 @@ public class DestinyMod
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+            event.accept(Moditems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
